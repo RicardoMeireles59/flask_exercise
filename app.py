e062e3846ad91a53192919014be48ed2343ba58a
Ricardo Meireles <ricardo.meireles31@gmail.com>
2026-03-18 20:43:26 -0300

    Ajuste das rotas do Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -18,5 +18,5 @@ def pokedex():
 def cadastro(): 
     return render_template("cadastro.html")
 
-if __name__ = "__main__"
+if __name__ == "__main__":
     app.run(debug=True)
