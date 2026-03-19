86ee2d28884912b1f80e4679b06fe28b5707d6ae
Ricardo Meireles <ricardo.meireles31@gmail.com>
2026-03-19 20:10:07 -0300

    Ajustes final de rotas

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home(): 
     return render_template("home.html")
     
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login(): 
     return render_template("login.html")
 
-@app.route("/pokedex")
+@app.route("/pokedex", methods=["GET"])
 def pokedex(): 
     return render_template("pokedex.html")
 
-@app.route("/cadastro")
-def cadastro(): 
+@app.route("/cadastro", methods=["GET", "POST"])
+def cadastro():
+
     return render_template("cadastro.html")
 
 if __name__ == "__main__":
